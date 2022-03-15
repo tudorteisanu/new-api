2d9f5e0d5c68853ee48c293d5956011ad604ec32
Tudor Teisanu <teisanutudort@gmail.com>
2022-03-16 00:20:20 +0200

    remove permission from git

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from src.app import app, FlaskConfig
 from src.services.logs import init_logging
 from src.services.http.permissions import save_permissions_to_file
 
+if FlaskConfig.PRODUCTION:
+    init_logging(app)
+save_permissions_to_file()
 
 if __name__ == '__main__':
-    if FlaskConfig.PRODUCTION:
-        init_logging(app)
-    save_permissions_to_file()
     app.run(FlaskConfig.HOST, FlaskConfig.PORT, debug=FlaskConfig.DEBUG)
