7636bdb6ff8464f899c7889c7fd70e068efc61f4
Tudor Teisanu <teisanutudort@gmail.com>
2022-03-20 17:59:05 +0200

    check permissions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-cd /var/www/html && python3 app.py --perms
+cd /var/www/html && python3 server.py --perms
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
 exit 0
