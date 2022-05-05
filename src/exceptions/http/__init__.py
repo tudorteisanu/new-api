2fd00ca04123a17c2be5fe33fecea2c1c458d077
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-05-05 10:11:19 +0300

    refactor http exceptions

--- src/exceptions/http/__init__.py
+++ src/exceptions/http/__init__.py
@@ -1,22 +1,3 @@
-class ValidationException(Exception):
-    def __init__(self, errors):
-        self.errors = errors
-
-
-class UnknownException(Exception):
-    def __init__(self, exception=None, message=None):
-
-        if message:
-            self.message = message
-
-        if exception:
-            self.exception = exception
-
-
-class NotFoundException(Exception):
-    message = None
-
-    def __init__(self, message=None):
-        if message:
-            self.message = message
-
+from .http_exceptions import UnknownException
+from .http_exceptions import ValidationException
+from .http_exceptions import NotFoundException
---
+++ src/exceptions/http/http_exceptions.py
@@ -0,0 +1,21 @@
+class ValidationException(Exception):
+    def __init__(self, errors):
+        self.errors = errors
+
+
+class UnknownException(Exception):
+    def __init__(self, exception=None, message=None):
+
+        if message:
+            self.message = message
+
+        if exception:
+            self.exception = exception
+
+
+class NotFoundException(Exception):
+    message = None
+
+    def __init__(self, message=None):
+        if message:
+            self.message = message
